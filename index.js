9731ac97b5836267abbf58ff4e78b2f32348df62
Ronnie Geraghty <ronniegerag@gmail.com>
2020-08-10 16:40:32 -0400

    Basics

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ require("dotenv").config();
 
 const token = process.env.TOKEN;
 
-const PREFIX = "#";
+const PREFIX = "*";
 
 bot.on("ready", () => {
   console.log("This bot is online");
 });
 
 bot.on("message", (msg) => {
-  if (msg.content === "HELLO") {
-    msg.reply("HELLO FRIEND!");
+  console.log("MESSAGE:", msg.content);
+  let args = msg.content.substring(PREFIX.length).split(" ");
+  switch (args[0]) {
+    case "ping":
+      msg.channel.send("pong!");
+      break;
+    case "website":
+      msg.channel.send("https://github.com/ronniegeraghty/Discord_Bot");
+      break;
+    case "info":
+      if (args[1] === "version") {
+        msg.channel.send("Version 0.0.0");
+      } else {
+        msg.channel.send("Invalid Args");
+      }
+      break;
+    case "clear":
+      if (!args[1]) return msg.reply("Error please define a second arg");
+      msg.channel.bulkDelete(args[1]);
+      break;
   }
 });
 
