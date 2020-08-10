9e85d408a71525ae7af37b42f77d1b56b41b13e0
Ronnie Geraghty <ronniegerag@gmail.com>
2020-08-10 18:28:11 -0400

    Added youtube audio play back

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 require("dotenv").config();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
 
 const token = process.env.TOKEN;
 
 const PREFIX = "*";
 
+var servers = {};
+
 bot.on("ready", () => {
   console.log("This bot is online");
 });
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   console.log("MESSAGE:", msg.content);
   let args = msg.content.substring(PREFIX.length).split(" ");
   switch (args[0]) {
     case "ping":
       msg.channel.send("pong!");
       break;
-    case "website":
-      msg.channel.send("https://github.com/ronniegeraghty/Discord_Bot");
+    case "embed":
+      console.log(msg.author.avatarURL);
+      const embed = new Discord.MessageEmbed()
+        .setTitle("User Information")
+        .addField("Current Server", msg.guild.name)
+        .addField("Player Name", msg.author.username)
+        .setColor(0xf1c40f)
+        .setThumbnail(msg.author.avatarURL());
+      msg.channel.send(embed);
       break;
-    case "info":
-      if (args[1] === "version") {
-        msg.channel.send("Version 0.0.0");
-      } else {
-        msg.channel.send("Invalid Args");
+    case "play":
+      function play(connection, msg) {
+        var server = servers[msg.guild.id];
+        server.dispatcher = connection.play(
+          ytdl(server.queue[0], { filter: "audioonly" })
+        );
+        server.queue.shift();
+        server.dispatcher.on("end", () => {
+          if (server.queue[0]) {
+            play(connection, msg);
+          } else {
+            connection.disconnect();
+          }
+        });
       }
+
+      if (!args[1]) {
+        msg.reply("You need to provide a link.");
+        break;
+      }
+      if (!msg.member.voice.channel) {
+        msg.reply("You must be in a voice channel to play music.");
+        break;
+      }
+      if (!servers[msg.guild.id]) servers[msg.guild.id] = { queue: [] };
+      var server = servers[msg.guild.id];
+      server.queue.push(args[1]);
+      msg.member.voice.channel.join().then((connection) => {
+        play(connection, msg);
+      });
+
       break;
-    case "clear":
-      if (!args[1]) return msg.reply("Error please define a second arg");
-      msg.channel.bulkDelete(args[1]);
-      break;
+    case "should":
+      msg.channel.send("No he's a lil bitch.");
   }
 });
 
