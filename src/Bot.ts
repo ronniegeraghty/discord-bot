c4e5d16daca1ec1b3d208e8f06a6d52d6996c95b
Ronnie Geraghty <ronniegerag@gmail.com>
2020-08-13 17:34:57 -0400

    Basic TypeScript version set up, Added on ReadyListener & PingCommand

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,1 +1,5 @@
 import { token, owners } from "./Config";
+import BotClient from "./client/BotClient";
+
+const client: BotClient = new BotClient({ token, owners });
+client.start();
--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -17,8 +17,54 @@ interface BotOptions {
 
 export default class BotClient extends AkairoClient {
   public config: BotOptions;
+  public listenerHandler: ListenerHandler = new ListenerHandler(this, {
+    directory: join(__dirname, "..", "listeners"),
+  });
   public commandHanlder: CommandHandler = new CommandHandler(this, {
     directory: join(__dirname, "..", "commands"),
     prefix: prefix,
+    allowMention: true,
+    handleEdits: true,
+    commandUtil: true,
+    commandUtilLifetime: 3e5,
+    defaultCooldown: 6e4,
+    argumentDefaults: {
+      prompt: {
+        modifyStart: (_: Message, str: string) =>
+          `${str}\n\nType \'cancel\' to cancel the command...`,
+        modifyRetry: (_: Message, str: string) =>
+          `${str}\n\nType \'cancel\' to cancel the command...`,
+        timeout: "You took to long, command has now been canceled...",
+        ended:
+          "You exceeded the maximum amount of tries, this command has now been cancelled...",
+        retries: 3,
+        time: 3e4,
+      },
+      otherwise: "",
+    },
+    ignorePermissions: owners,
   });
+
+  public constructor(config: BotOptions) {
+    super({
+      ownerID: config.owners,
+    });
+    this.config = config;
+  }
+
+  private async _init(): Promise<void> {
+    this.commandHanlder.useListenerHandler(this.listenerHandler);
+    this.listenerHandler.setEmitters({
+      commandHandler: this.commandHanlder,
+      listenerHandler: this.listenerHandler,
+      process,
+    });
+    this.commandHanlder.loadAll();
+    this.listenerHandler.loadAll();
+  }
+
+  public async start(): Promise<string> {
+    await this._init();
+    return this.login(this.config.token);
+  }
 }
