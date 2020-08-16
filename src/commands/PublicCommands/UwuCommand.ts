2be47a20e5084136ade52c2063b9d0710f5f302d
Ronnie Geraghty <ronniegerag@gmail.com>
2020-08-16 02:16:55 -0400

    Added UwuCommand

---
+++ src/commands/PublicCommands/UwuCommand.ts
@@ -0,0 +1,27 @@
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+
+export default class UwuCommand extends Command {
+  public constructor() {
+    super("uwu", {
+      aliases: ["uwu"],
+      category: "Public Command",
+      description: {
+        content: "Shows you our server wifu",
+        usage: "uwu",
+        examples: ["uwu"],
+      },
+      ratelimit: 3,
+    });
+  }
+  public exec(message: Message): Promise<Message> {
+    return message.util.send(
+      new MessageEmbed()
+        .setTitle(`WIFU`)
+        .setColor("#FFC0CB")
+        .setImage(
+          `https://pm1.narvii.com/5689/ac893cb77bc91c4f581500beb77b201f525078e5_hq.jpg`
+        )
+    );
+  }
+}
