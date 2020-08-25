dd807873602ea80a2dfdc68fbe9ea2f763e40561
Ronnie Geraghty <ronniegerag@gmail.com>
2020-08-25 14:40:30 -0400

    Added context comments for PauseCommand

--- src/commands/PublicCommands/PauseCommand.ts
+++ src/commands/PublicCommands/PauseCommand.ts
@@ -16,11 +16,14 @@ export default class PauseCommand extends Command {
     });
   }
   public execute(message: Message): Promise<Message> {
+    //Get dispatcher for current voice channel
     let dispatcher = this.client.getDispatcher(message.member.voice.channel);
+    //If no dispatcher found, send reply
     if (!dispatcher)
       return message.util.reply(
         `Either no music is playing or you are not in a voice channel where music is playing.`
       );
+    //If there is a dispatcher, pause it.
     else {
       dispatcher.pause(true);
     }
