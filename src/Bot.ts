317970ff4a453a5886d57daea5b7a01de0ce02b9
Ronnie Geraghty <ronniegerag@gmail.com>
2021-12-08 15:03:17 -0800

    clearing src for update to discord.js 13 and dropping akairo

--- src/Bot.ts
+++
@@ -1,6 +0,0 @@
-import { token, owners, prefix } from "./Config";
-import BotClient from "./client/BotClient";
-
-const client: BotClient = new BotClient({ prefix, token, owners });
-console.log(`Starting Discord Bot`);
-client.start();
