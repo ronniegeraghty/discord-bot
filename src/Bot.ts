1b0b9d075df4f5e747306d87a77d28111053d691
Ronnie Geraghty <ronniegerag@gmail.com>
2021-12-08 16:00:51 -0800

    finished creating commands section

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,4 +1,4 @@
-import { Client, Intents } from "discord.js";
+import { Client, Intents, Interaction, Message } from "discord.js";
 import { token } from "./config.json";
 
 // Create instance of client
@@ -7,6 +7,23 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 //When the client is ready, run this code(only once)
 client.once("ready", () => {
   console.log(`${client.user.tag} is Ready!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+  if (commandName === "ping") {
+    await interaction.reply(`Pong! - ${client.ws.ping}ms`);
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  }
 });
 
 // Login to Discord with your client's token
