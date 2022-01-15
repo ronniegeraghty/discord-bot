9f80113568df17e72e933e7de58477b5c531b29e
Ronnie Geraghty <ronniegerag@gmail.com>
2022-01-15 00:08:25 -0800

    Updated bot to only use secrets from environment and not from config.json file

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,21 +1,24 @@
-import { token, databaseConfig, rawCommandOptions } from "./config.json";
 import BotClient from "./client/BotClient";
 import { Intents } from "discord.js";
 import * as dotenv from "dotenv";
+import { DatabaseOptions } from "./database/DatabaseOptions.type";
+import { RawCommandOptions } from "./client/Command";
 dotenv.config();
 
-let dbConfig = databaseConfig;
+const token = process.env.DISCORD_TOKEN;
+const dbConfig: DatabaseOptions = {
+  username: process.env.MONGO_ROOT_USER,
+  password: process.env.MONGO_ROOT_PASSWORD,
+  url:
+    process.env.ENV === "CONTAINER" ? process.env.MONGO_HOST_NAME : "localhost",
+  port: process.env.MONGO_INTERNAL_PORT,
+  dbName: process.env.MONGO_DATABASE,
+  dbOptions: process.env.MONGO_OPTIONS,
+};
 
-//Check if Prod Env and if Prod change config options
-if (process.env.ENV === "PROD") {
-  console.log(`Configuring for PROD`);
-  dbConfig = {
-    ...databaseConfig,
-    url: process.env.MONGO_HOST_NAME,
-    dbOptions: "authSource=admin",
-  };
-}
-console.log("DB CONFIG: ", dbConfig);
+const rawCommandOptions: RawCommandOptions = {
+  prefix: process.env.PREFIX,
+};
 
 // Create instance of client
 const client = new BotClient(token, dbConfig, rawCommandOptions, {
