518c4410e220527534aba8c460dd3dfa1794c7f1
Ronnie Geraghty <ronniegerag@gmail.com>
2022-01-26 13:22:10 -0800

    built of template custom action

--- .github/actions/create-docker-tag/index.js
+++ .github/actions/create-docker-tag/index.js
@@ -1,1 +1,14 @@
-console.log("Hello from github action");
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  // `image` input defined in action metadata file
+  const baseImageName = core.getInput("image");
+  console.log(`Base Image Name: ${baseImageName}`);
+  core.setOutput("tag", baseImageName);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
