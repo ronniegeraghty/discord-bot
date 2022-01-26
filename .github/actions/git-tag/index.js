50263ff8e038149b7e878210472410f471ae2d14
Ronnie Geraghty <ronniegerag@gmail.com>
2022-01-26 14:38:48 -0800

    renamed custom action added req to get tags

--- .github/actions/git-tag/index.js
+++ .github/actions/git-tag/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-// const http = requier("http");
+const https = require("https");
 
 try {
   // // `image` input defined in action metadata file
@@ -8,15 +8,18 @@ try {
   // console.log(`Base Image Name: ${baseImageName}`);
   // core.setOutput("tag", baseImageName);
 
-  //Get Next patch tag
-  // const options = {
-  //   url:
-  //   method: 'GET'
-  // }
-  // let req = http.get();
+  const tagUrl = github.context.payload.repository["tags_url"];
+  console.log(`Tags URL: ${tagUrl}`);
+
+  let req = https.request(tagUrl, (res) => {
+    console.log(`Status Code: ${res.statusCode}`);
+    res.on("data", (d) => {
+      process.stdout.write(d);
+    });
+  });
+  req.end();
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  console.log(`Tags URL: ${github.context.payload.repository["tags_url"]}`);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
