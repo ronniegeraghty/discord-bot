733bef6356ab2698ad84d6702d55534015c336aa
Ronnie Geraghty <ronniegerag@gmail.com>
2022-01-26 14:03:42 -0800

    test

--- .github/actions/create-docker-tag/index.js
+++ .github/actions/create-docker-tag/index.js
@@ -1,13 +1,26 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const http = requier("http");
 
 try {
-  // `image` input defined in action metadata file
-  const baseImageName = core.getInput("image");
-  console.log(`Base Image Name: ${baseImageName}`);
-  core.setOutput("tag", baseImageName);
+  // // `image` input defined in action metadata file
+  // const baseImageName = core.getInput("image");
+  // console.log(`Base Image Name: ${baseImageName}`);
+  // core.setOutput("tag", baseImageName);
+
+  //Get Next patch tag
+  // const options = {
+  //   url:
+  //   method: 'GET'
+  // }
+  // let req = http.get();
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  const payload = JSON.stringify(
+    github.context.payload.base.repo.tags_url,
+    undefined,
+    2
+  );
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
