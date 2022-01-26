012cc05ab058a198b4695c3df7f0d7b8f41b310f
Ronnie Geraghty <ronniegerag@gmail.com>
2022-01-26 14:18:55 -0800

    test

--- .github/actions/create-docker-tag/index.js
+++ .github/actions/create-docker-tag/index.js
@@ -16,11 +16,8 @@ try {
   // let req = http.get();
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(
-    github.context.payload.base.repo.tags_url,
-    undefined,
-    2
-  );
+  console.log(`Tags URL: ${github.context.payload.repository["tags_url"]}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
