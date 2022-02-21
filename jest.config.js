0c876a1adbdebc2e84662982c47e57cbbdc0a96c
Ronnie Geraghty <ronniegerag@gmail.com>
2022-02-21 09:07:11 -0800

    init and configed jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["./src"],
+  testMatch: [
+    "**/__test__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
